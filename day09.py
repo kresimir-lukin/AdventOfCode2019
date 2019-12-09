6e59ebb1f22be1a3cfb5be5ed9ad8fc1f0f4692d
Kresimir Lukin <klukinfree@gmail.com>
2019-12-09 16:55:38 +0400

    Added solution for day 9
    Refactored IntCode module
    Simplified solution for day 5

--- day09.py
+++ day09.py
@@ -4,6 +4,6 @@ assert len(sys.argv) == 2
 code = list(map(int, open(sys.argv[1]).read().split(',')))
 
 part1 = intcode.IntCode(code, [1]).execute()
-part2 = intcode.IntCode(code, [5]).execute()
+part2 = intcode.IntCode(code, [2]).execute()
 
 print('Part 1: {0}, Part 2: {1}'.format(part1, part2))
--- intcode.py
+++ intcode.py
@@ -2,52 +2,60 @@ class Instruction:
     code, argument_num = 0, 0
     def execute(self, intcode, arguments):
         pass
+    def new_pc(self, intcode):
+        return intcode.pc + self.argument_num + 1
 
 class PlusInstruction(Instruction):
     code, argument_num = 1, 3
     def execute(self, intcode, arguments):
         intcode.update(arguments[2].address, arguments[0].value + arguments[1].value)
-        return intcode.pc + 4
+        return self.new_pc(intcode)
 
 class MultiplyInstruction(Instruction):
     code, argument_num = 2, 3
     def execute(self, intcode, arguments):
         intcode.update(arguments[2].address, arguments[0].value * arguments[1].value)
-        return intcode.pc + 4
+        return self.new_pc(intcode)
 
 class InputInstruction(Instruction):
     code, argument_num = 3, 1
     def execute(self, intcode, arguments):
         intcode.update(arguments[0].address, intcode.pop_input())
-        return intcode.pc + 2
+        return self.new_pc(intcode)
 
 class OutputInstruction(Instruction):
     code, argument_num = 4, 1
     def execute(self, intcode, arguments):
-        intcode.output = intcode.get(arguments[0].address)
-        return intcode.pc + 2
+        intcode.output = arguments[0].value
+        return self.new_pc(intcode)
 
 class JumpIfTrueInstruction(Instruction):
     code, argument_num = 5, 2
     def execute(self, intcode, arguments):
-        return arguments[1].value if arguments[0].value != 0 else intcode.pc + 3
+        return arguments[1].value if arguments[0].value != 0 else self.new_pc(intcode)
 
 class JumpIfFalseInstruction(Instruction):
     code, argument_num = 6, 2
     def execute(self, intcode, arguments):
-        return arguments[1].value if arguments[0].value == 0 else intcode.pc + 3
+        return arguments[1].value if arguments[0].value == 0 else self.new_pc(intcode)
 
 class LessThanInstruction(Instruction):
     code, argument_num = 7, 3
     def execute(self, intcode, arguments):
         intcode.update(arguments[2].address, int(arguments[0].value < arguments[1].value))
-        return intcode.pc + 4
+        return self.new_pc(intcode)
 
 class EqualsInstruction(Instruction):
     code, argument_num = 8, 3
     def execute(self, intcode, arguments):
         intcode.update(arguments[2].address, int(arguments[0].value == arguments[1].value))
-        return intcode.pc + 4
+        return self.new_pc(intcode)
+        
+class RelativeBaseOffsetInstruction(Instruction):
+    code, argument_num = 9, 1
+    def execute(self, intcode, arguments):
+        intcode.update_relative_base(arguments[0].value)
+        return self.new_pc(intcode)
 
 class HaltInstruction(Instruction):
     code, argument_num = 99, 0
@@ -55,14 +63,16 @@ class HaltInstruction(Instruction):
         return None
 
 class Argument:
-    def __init__(self, address, value):
-        self.address = address
+    def __init__(self, value, address):
         self.value = value
-
+        self.address = address
+        
 class IntCode:
     def __init__(self, program, inputs):
         self.program = program[:]
         self.inputs = inputs[::-1]
+        self.relative_base = 0
+        self.memory = {}
         self.output = None
         self.pc = 0
 
@@ -73,8 +83,11 @@ class IntCode:
         modes = str(self.program[self.pc]).zfill(5)[:3][::-1]
         arguments = []
         for i in range(argument_num):
-            address = self.program[self.pc+i+1]
-            arguments.append(Argument(address, self.program[address] if modes[i] == '0' else address))
+            value = self.program[self.pc+i+1] + (self.relative_base if modes[i] == '2' else 0)
+            if modes[i] == '1':
+                arguments.append(Argument(value, value))
+            else:
+                arguments.append(Argument(self.program[value] if value < len(self.program) else self.memory.get(value, 0), value))
         return arguments
 
     def run(self):
@@ -85,14 +98,27 @@ class IntCode:
             self.pc = instruction().execute(self, arguments)
         return self.pc is None
 
+    def execute(self):
+        last_output, halted = None, False
+        while not halted:
+            last_output = self.output
+            halted = self.run()
+        return last_output
+
     def get(self, address):
-        return self.program[address]
+        return self.program[address] if address < len(self.program) else self.memory.get(value + self.relative_base, 0)
 
     def update(self, address, value):
-        self.program[address] = value
+        if address < len(self.program):
+            self.program[address] = value
+        else:
+            self.memory[address] = value
 
     def pop_input(self):
         return self.inputs.pop()
 
     def add_input(self, value):
         self.inputs = [value] + self.inputs
+    
+    def update_relative_base(self, value):
+        self.relative_base += value
